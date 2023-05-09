285529b6ed8eee91b485be9a59c99b0442e7d0da
Tilen Vidmar <116103801+vidmartilen04@users.noreply.github.com>
2023-05-09 12:23:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/ico" href="img/favicon.ico">
+	<link rel="stylesheet" href="style.css">
+	<title>Gambling</title>
+</head>
+<body>
+	<div id=head>
+	<h1 id="title">VIVA LAS VEGAS</h1>
+	</div>
+	<div id="body">
+		<br>
+		<h2 id="players">Players</h2>
+		<div id="PlayersForm">
+			<form>
+				<label for="Player1">Player 1:</label>
+				<input type="text" name="Player1">
+				<label for="Player2">Player 2:</label>
+				<input type="text" name="Player2">
+				<label for="Player3">Player 3:</label>
+				<input type="text" name="Player3">
+				<input id="button" type="submit" name="Confirm Players" value="Confirm Players">
+			</form>
+		</div>
+	</div>
+</body>
+</html>
