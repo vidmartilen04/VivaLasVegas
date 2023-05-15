567bd689ad8e7c5d59aa7fdb6528639a32cd2e8e
Tilen Vidmar <116103801+vidmartilen04@users.noreply.github.com>
2023-05-15 14:56:36 +0200

    Add files via upload

---
+++ game.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/ico" href="img/favicon.ico">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="styleGame.css">
+	<title>Viva Las Vegas</title>
+</head>
+<body>
+	<div id=head>
+	<h1 id="title">VIVA LAS VEGAS</h1>
+	</div>
+	<div id="body">
+		<br>
+		<div id="players">
+		<div id="player1">
+			<p>Player 1</p>
+			<img class="diceAnimation" src="img/dicee.gif">
+			<img class="diceAnimation" src="img/dicee.gif">
+
+			<p>Number rolled:</p>
+			<p>Total:</p>
+
+		</div>
+		<div id="player2">
+			<p>Player 2</p>
+			<img class="diceAnimation" src="img/dicee.gif">
+			<img class="diceAnimation" src="img/dicee.gif">
+
+			<p>Number rolled:</p>
+			<p>Total:</p>
+		</div>
+		<div id="player3">
+			<p>Player 3</p>
+			<img class="diceAnimation" src="img/dicee.gif">
+			<img class="diceAnimation" src="img/dicee.gif">
+
+			<p>Number rolled:</p>
+			<p>Total:</p>
+		</div>
+
+	</div>
+	</div>
+</body>
+</html>
+<?php 
+
+
+?>
--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/ico" href="img/favicon.ico">
 	<link rel="stylesheet" href="style.css">
-	<title>Gambling</title>
+	<script src="sweetalert2.min.js"></script>
+	<link rel="stylesheet" href="sweetalert2.min.css">
+	<title>Viva Las Vegas</title>
 </head>
 <body>
 	<div id=head>
@@ -13,18 +15,49 @@
 	</div>
 	<div id="body">
 		<br>
-		<h2 id="players">Players</h2>
-		<div id="PlayersForm">
-			<form>
+			<form action="game.php" method="POST">
+				<div id="dForm">
+				<p id="players">Players</p>	
 				<label for="Player1">Player 1:</label>
-				<input type="text" name="Player1">
-				<label for="Player2">Player 2:</label>
+				<input type="text" name="Player1" required>
+				<label for="Player2">Player 2:</label required>
 				<input type="text" name="Player2">
-				<label for="Player3">Player 3:</label>
+				<label for="Player3">Player 3:</label required>
 				<input type="text" name="Player3">
-				<input id="button" type="submit" name="Confirm Players" value="Confirm Players">
+			</div>
+			<br>
+			<div id="dForm">
+				<p id="players">Number of dice</p>
+				<label for="Player">1 dice</label>
+				<input type="radio" name="Player1" value="1">
+				<label for="Player">2 dice</label>
+				<input type="radio" name="Player2" value="2">
+				<label for="Player">3 dice</label>
+				<input type="radio" name="Player3" value="3">
+			</div>
+			<br>
+			<div id="dForm">
+				<p id="players">Select number of rounds</p>
+				<select name="rounds" id="playersS">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
+				</select>
+			</div>
+			<br>
+			<center><input id="button" type="submit" name="Confirm Players" value="Let's GAMBLE"></center>
 			</form>
-		</div>
 	</div>
 </body>
 </html>
+<?php 
+
+
+?>
